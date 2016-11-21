0195ea73e4a696c3e39cf61735f79e24fd8e613d
flor5112 <miflores@csumb.edu>
2016-11-21 14:00:32 -0800

    implemented simple HTTP POST request to create pet entry in DB

--- petTracker/ViewController.swift
+++ petTracker/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var petName: UITextField!
+    @IBOutlet weak var petDescription: UITextField!
+    @IBOutlet weak var DOB: UITextField!
+    
+    @IBAction func addPet(sender: UIButton) {
+        
+        //get textbox values
+        let petNameValue = petName.text
+        let petDescriptionValue = petDescription.text
+        let petDOBValue = DOB.text
+        
+        //connect to server
+        let petUrl = NSURL(string: "https://pettrackerapp.herokuapp.com/pet/create")
+        let request = NSMutableURLRequest(URL:petUrl!)
+        request.HTTPMethod = "POST"
+        let postString = "petName=" + petNameValue! +
+                         "&type=" + petDescriptionValue! +
+                         "&dob=" + petDOBValue!
+        //start session/request
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
+            data, response, error in
+            
+            if error != nil {
+                print("error\(error)")
+                return
+            }
+            print("******Response =\(response)")
+            
+        
+        }
+        
+        task.resume()
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +57,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
